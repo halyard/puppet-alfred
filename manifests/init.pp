d2d4e461bb04a7b674fcd0ef39bd19ab82acc97a
Les Aker <me@lesaker.org>
2015-06-12 15:53:20 -0400

    add support for launching alfred

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,18 @@
 #
 class alfred (
   $package = 'halyard/casks/alfred-halyard'
+  $install_path = "/Users/${::boxen_user}/Applications/Alfred 2.app"
 ) {
   package { $package:
     provider => 'brewcask',
     require  => Homebrew::Tap['halyard/casks']
+  }  ->
+  osx_login_item { 'Alfred':
+    hidden => true,
+    path   => $install_path
+  } ~>
+  exec { "launch alfred":
+    command     => "/usr/bin/open '${install_path}'",
+    refreshonly => true,
   }
 }
