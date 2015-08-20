feb0adadd5c38ecd31425677c14e345832918a06
Les Aker <me@lesaker.org>
2015-08-20 19:56:45 -0400

    first pass at dropbox-binding

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,12 +9,22 @@
 #
 class alfred (
   $package = 'alfred-halyard',
-  $install_path = "/Users/${::boxen_user}/Applications/Alfred 2.app"
+  $install_path = "/Users/${::boxen_user}/Applications/Alfred 2.app",
+  $license_src = 'licenses/alfred',
+  $license_dst = "/Users/${::boxen_user}/Library/Application Support/Alfred 2/license.plist"
 ) {
+  dropbox::synced_file { $license_src: } ~>
+  file { $license_dst:
+    ensure  => 'present',
+    content => dropbox_file($license_src)
+  } ->
   package { $package:
     provider => 'brewcask',
-    require  => Homebrew::Tap['halyard/casks']
-  }  ->
+    require  => [
+      Homebrew::Tap['halyard/casks'],
+      Package['dropbox-halyard']
+    ]
+  } ->
   osx_login_item { 'Alfred 2':
     hidden => true,
     path   => $install_path
