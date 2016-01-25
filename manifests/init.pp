5d74af905f7e9c4d99e0d4883ee09fba28560242
Les Aker <me@lesaker.org>
2016-01-24 19:30:45 -0500

    remove dropbox magic

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,21 +5,10 @@
 class alfred (
   $package = 'alfred-halyard',
   $install_path = "/Users/${::boxen_user}/Applications/Alfred 2.app",
-  $license_src = 'licenses/alfred',
-  $license_dst =
-    "/Users/${::boxen_user}/Library/Application Support/Alfred 2/license.plist"
 ) {
-  dropbox::synced_file { $license_src: } ~>
-  file { $license_dst:
-    ensure  => 'present',
-    content => dropbox_file($license_src)
-  } ->
   package { $package:
     provider => 'brewcask',
-    require  => [
-      Homebrew::Tap['halyard/casks'],
-      Package['dropbox-halyard']
-    ]
+    require  => Homebrew::Tap['halyard/casks']
   } ->
   osx_login_item { 'Alfred 2':
     hidden => true,
